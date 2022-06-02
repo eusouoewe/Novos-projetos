471428f8a5b060913a5a79662107b14c683ded60
eusouoewe <ewe.greed25@gmail.com>
2022-06-02 12:03:02 -0300

    Mudanças
    Adicionei mais funçoes, realizei algumas mudanças, perdi a paciência, depois continuo

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -1,9 +1,33 @@
 const  cellElements = document.querySelectorAll("[data-cell]");
+const field = document.querySelectorAll("[data-field]");
 
-let isCircleTurn = false;
+let isCircleTurn;
+
+const startGame = () => {
+        for (const cell of cellElements) {
+                cell.addEventListener("click", handleClick, {once: true});
+        }
+
+        isCircleTurn = false;
+
+        field.classList.add("x");
+}
 
 const placeMark = (cell, classToAdd) => {
         cell.classList.add(classToAdd);
+}
+
+const swapTurns = () => {
+        isCircleTurn = !isCircleTurn;
+
+        field.classList.remove('circle');
+        field.classList.remove('x');
+
+        if (isCircleTurn) {
+                field.classList.add('circle');
+        } else {
+                field.classList.add('x');
+        }
 }
 
 const handleClick = (e) => {
@@ -11,12 +35,13 @@ const handleClick = (e) => {
         const cell = e.target;
         const classToAdd = isCircleTurn ? 'circle' : 'x';
 
-        cell.classList.add(classToAdd);
+        placeMark(cell, classToAdd);
+
         // verify victory
         // verify draw
         //change symbol
+
+        swapTurns ();
 }
 
-for (const cell  of cellElements) {
-        cell.addEventListener('click', handleClick, { once: true });
-}
+startGame();
